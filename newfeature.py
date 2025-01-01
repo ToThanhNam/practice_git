ef2350d8531818c4cc3e7fd9af14565810f132a8
NamTT <namtt@rikkeisoft.com>
2025-01-01 23:05:59 +0700

    new feature to master

---
+++ newfeature.py
@@ -0,0 +1,11 @@
+def print_personal_info():
+    name = "New feature"
+    age = 30
+    city = "New feature"
+    
+    print("Name:", name)
+    print("Age:", age)
+    print("City:", city)
+
+# Call the function to display the information
+print_personal_info()
