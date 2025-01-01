6d8c50156a2bf8cccae3e8ff377fb207af355109
NamTT <namtt@rikkeisoft.com>
2025-01-01 22:22:16 +0700

    namtest.py

---
+++ namtest.py
@@ -0,0 +1,11 @@
+def print_personal_info():
+    name = "John Doe"
+    age = 30
+    city = "New York"
+    
+    print("Name:", name)
+    print("Age:", age)
+    print("City:", city)
+
+# Call the function to display the information
+print_personal_info()
