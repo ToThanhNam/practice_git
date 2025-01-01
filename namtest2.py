9926e3755af76be72b61b2ec84d9dc3fe0d00312
NamTT <namtt@rikkeisoft.com>
2025-01-01 22:58:46 +0700

    reset to commit 1

---
+++ namtest2.py
@@ -0,0 +1,11 @@
+def print_personal_info():
+    name = "Nam test 2"
+    age = 30
+    city = "Nam test 2"
+    
+    print("Name:", name)
+    print("Age:", age)
+    print("City:", city)
+
+# Call the function to display the information
+print_personal_info()
